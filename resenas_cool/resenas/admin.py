261e1de9fd720cc877f86ea110df3231ccc570d1
Pedro Astaburuaga <pedroastaburuaga14@gmail.com>
2023-04-25 21:12:42 -0400

    Modelo Categorias y Reseñas

---
+++ resenas_cool/resenas/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from resenas.models import Categorias, Resenas
+
+admin.site.register(Categorias)
+admin.site.register(Resenas)
---
+++ resenas_cool/resenas/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from usuarios.models import Usuario
+
+class Categorias(models.Model):
+    id = models.AutoField(primary_key=True)  
+    nombre = models.CharField(max_length=25)
+    
+    #categoria = models.ForeignKey(Categoria, default="general", on_delete=models.CASCADE)  # la llave foránea
+
+    def __str__(self):
+        return self.nombre # name to be shown when called
+    
+
+class Resenas(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre_producto = models.CharField(max_length=25)
+    fecha_creación = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    titulo = models.CharField(max_length=25)
+    descripcion = models.TextField(blank=True)
+
+    id_categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    foto = models.ImageField()
+
+    def __str__(self):
+        return self.titulo  # name to be shown when called
