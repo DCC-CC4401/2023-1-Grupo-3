63dd60d54ed3b71351a15792b6d3703e51a832b1
Demian Hott <demian.hott@ug.uchile.cl>
2023-04-26 19:37:37 -0400

    se modifico la estetica del template

--- resenas_cool/usuarios/login.py
+++ resenas_cool/usuarios/login.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class NuevaTareaForm(forms.Form):
-   nombre = forms.CharField(max_length=25)
-   contrasenna = forms.CharField(max_length=25)
+   nombre = forms.CharField(max_length=25, required=True)
+   contraseña = forms.CharField(max_length=25, required=True, widget=forms.PasswordInput())
