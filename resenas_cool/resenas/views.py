ef4926c6b96152c7e38d4fc77c93997ad439c4bd
Kathleen Kohler <katiikohler@gmail.com>
2023-05-07 20:46:19 -0400

    se modifica views, forms y se cambia html de reseñas

--- resenas_cool/resenas/views.py
+++ resenas_cool/resenas/views.py
@@ -5,21 +5,31 @@ from resenas.models import Resenas, Categorias
 
 def nueva_resena(request):
   
+  #agregar if is_authenticated
+  #si no, redirigir a login
+  
   if request.method == "GET":
      form_resena = NuevaResenaModelForm()
-     return render(request, "../templates/nueva_resena.html", {"form_resena": form_resena})
+     categorias =Categorias.objects.all()
+     return render(request, "../templates/nueva_resena.html", {"form_resena": form_resena, "categorias":categorias})
   
   if request.method == "POST":  
-     if "reviewAdd" in request.POST:  #crear boton
-        nombre_producto = request.POST["nombre_producto"]
-        titulo = request.POST["titulo"] 
-        nombre_categoria = request.POST["selector_categoria"]  
-        categoria = Categorias.objects.get(nombre=nombre_categoria)
-        descripcion = request.POST["descripcion"]
-        foto = request.POST["foto"]
+      
+      form_resena = NuevaResenaModelForm(request.POST)
+      if form_resena.is_valid():
+         form_resena.save()
+
+       #if "reviewAdd" in request.POST:  #crear boton
+       #  nombre_producto = request.POST["nombre_producto"]
+       # titulo = request.POST["titulo"] 
+        #nombre_categoria = request.POST["selector_categoria"]  
+        #categoria = Categorias.objects.get(nombre=nombre_categoria)
+        #descripcion = request.POST["descripcion"]
+        #foto = request.POST["foto"]
         
-        nueva_resena = Resenas(nombre_producto=nombre_producto, titulo=titulo, descripcion=descripcion, id_categoria=categoria.id, id_usuario=request.user, foto=foto)
-        if nueva_resena.is_valid():
-           nueva_resena.save()
-           
-        return redirect('/resenas')
+        #nueva_resena = Resenas(nombre_producto=nombre_producto, titulo=titulo, descripcion=descripcion, id_categoria=categoria.id, id_usuario=request.user, foto=foto)
+        #if nueva_resena.is_valid():
+         #  nueva_resena.save()
+
+      categorias =Categorias.objects.all()#hay que sacarlo cuando cambiemos el render   
+      return render(request, "../templates/nueva_resena.html", {"form_resena": form_resena, "categorias":categorias})
