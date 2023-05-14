bd1cf8c5a889573c3e40d320401bb9289a72172d
Demian Hott <demian.hott@ug.uchile.cl>
2023-05-14 18:56:30 -0400

    se agrega el template de inicio y se modifican los urls para su correcta funcionalidad

--- resenas_cool/inicio/views.py
+++ resenas_cool/inicio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from resenas.models import Resena, Categorias
+from resenas.models import Resenas, Categorias
 
-
+ 
 def ver_resenas(request):
     if request.method == 'GET':
-        return render(request, 'ver_resenas.html', {"categorias": Categorias.objects.all(), "resenas": Resena.objects.all()})
+        return render(request, 'inicio/inicio.html', {"categorias": Categorias.objects.all(), "resenas": Resenas.objects.all()})
