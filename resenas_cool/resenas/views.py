6fdda73038fb12e6decc809f52ded0a43e1f35e6
valepazg <valeria.paz.gutierrez@gmail.com>
2023-06-12 19:46:45 -0400

    arreglo foto mod resena

--- resenas_cool/resenas/views.py
+++ resenas_cool/resenas/views.py
@@ -81,11 +81,12 @@ def modificar_resena(request, review_id):
             nombre_categoria = request.POST["selector_categoria"]
             nueva_categoria = Categorias.objects.get(nombre=nombre_categoria)
             nueva_descripcion = request.POST.get('descrpcion')
-            nueva_foto = request.FILES['foto']
+            nueva_foto = request.FILES.get('foto')
 
             # Actualizar los campos de la instancia de la reseña
             resena.nombre_producto = nuevo_nombre_producto if nuevo_nombre_producto is not None else resena.nombre_producto 
             print(nuevo_nombre_producto)
+            print(nueva_foto)
             resena.titulo = nuevo_titulo if nuevo_titulo is not None else resena.titulo
             resena.id_categoria = nueva_categoria if nueva_categoria is not None else resena.id_categoria
             resena.descripcion = nueva_descripcion if nueva_descripcion is not None else resena.descripcion
